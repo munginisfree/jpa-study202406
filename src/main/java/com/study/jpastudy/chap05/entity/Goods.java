5c7b42bf34870dc7ab0772184d882f1db2e6841d
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-27 10:15:38 +0900

    # 다대다 관계 엔터티 클래스 설계

---
+++ src/main/java/com/study/jpastudy/chap05/entity/Goods.java
@@ -0,0 +1,29 @@
+package com.study.jpastudy.chap05.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+@Setter @Getter
+@ToString(exclude = "purchaseList")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_mtm_goods")
+public class Goods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "goods_id")
+    private Long id;
+
+    @Column(name = "goods_name")
+    private String name;
+
+    @OneToMany(mappedBy = "goods", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Purchase> purchaseList = new ArrayList<>();
+}
---
+++ src/main/java/com/study/jpastudy/chap05/entity/Purchase.java
@@ -0,0 +1,31 @@
+package com.study.jpastudy.chap05.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@ToString(exclude = {"user", "good"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_mtm_purchase")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "purchase_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goods_id")
+    private Goods good;
+}
